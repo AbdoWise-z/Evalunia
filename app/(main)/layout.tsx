bd0a21bf47da8ed7b4bd4e71211758b5d8dc7bf4
Aryan-Bhargav8 <bhargav.aryan8@gmail.com>
2024-08-25 15:44:40 +0530

    made layout resposive and fixed console error of browsers

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -19,7 +19,7 @@ const MainLayout = async (
       <SideNav currentUser={user} />
       <div className="flex flex-1">
         <div
-          className="p-2 md:p-4 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-neutral-200 dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full overflow-y-auto"
+          className="p-2 md:p-4 rounded-tl-2xl border border-neutral-200 dark:border-neutral-700 bg-neutral-200 dark:bg-neutral-900 flex flex-col gap-2 flex-1 w-full h-full"
         >
           {children}
         </div>
