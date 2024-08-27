101e54b9f8fdc5e2c6f2e7d61f60a741b0889d97
AbdoWise-z <moh2002.abdow@gmail.com>
2024-08-27 11:56:08 +0300

    fixed route for the add page

--- components/nav/side-nav.tsx
+++ components/nav/side-nav.tsx
@@ -58,7 +58,7 @@ export function SideNav(
     },
     {
       label: "Add Professor",
-      href: "/prof",
+      href: "/add",
       icon: (
         <IconPlus className="text-neutral-700 dark:text-neutral-200 h-5 w-5 flex-shrink-0" />
       ),
